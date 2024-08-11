3ca62e63630e67dbcd0f6db12d1363c44f788f2d
xax-ale <xandemasc@gmail.com>
2024-08-11 18:00:34 -0300

    committed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://xax-ale.github.io',
+    base: '/Portifolio-de-projetos',
+});
