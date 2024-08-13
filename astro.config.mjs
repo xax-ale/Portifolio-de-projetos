5d6f5800c06e53af47b379f945ecad1d2b6ce17c
Alê Henrique <xandemasc@gmail.com>
2024-08-12 22:30:36 -0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,14 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
     site: 'https://xax-ale.github.io',
     base: '/Portifolio-de-projetos',
+  vite: {
+    build: {
+      rollupOptions: {
+        external: [
+          /public\/.*\.png$/, // Regex para tratar imagens PNG na pasta public
+          /public\/.*\.jpg$/, // Adicione outros formatos de imagem conforme necessário
+        ],
+      },
+    },
+  },
 });
